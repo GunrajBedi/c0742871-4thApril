2cc6c3cdbb8f1d41bd2ee93f27da28e6f4e2e863
Gunraj Singh Bedi <742871@student.tor.loc>
2019-04-04 10:07:23 -0400

    first commit

--- 4thapril.js
+++ 4thapril.js
@@ -1,1 +1,10 @@
 //this is js file
+console.log('heloo.....!!');
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('hello world');
+
+
+}).listen(8081);
